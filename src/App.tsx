8eff14ae3a2d8c74e781b3e9c96b80a2951692a5
Seokseong Jeon <seokseong@jeon.engineer>
2023-04-20 23:04:12 +0900

    Add CKEditor prototype

--- src/App.tsx
+++ src/App.tsx
@@ -1,33 +1,96 @@
-import { useState } from 'react'
-import './App.css'
+import "./App.css";
+import { CKEditor } from "@ckeditor/ckeditor5-react";
+import ClassicEditor from "@ckeditor/ckeditor5-build-classic";
+import { createRef } from "react";
 
 function App() {
-  const [count, setCount] = useState(0)
+  const refEditor = createRef<CKEditor<ClassicEditor>>();
+
+  const handleChange = () => {
+    if (!refEditor.current) {
+      return;
+    }
+    console.log(refEditor.current.editor?.data?.get());
+    console.log(
+      Array.from(refEditor.current.editor?.ui.componentFactory.names() ?? [])
+    );
+  };
 
   return (
     <div className="App">
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src="/vite.svg" className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://reactjs.org" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      <CKEditor
+        editor={ClassicEditor}
+        config={{
+          toolbar: [
+            "heading",
+            "|",
+            "bold",
+            "italic",
+            "|",
+            "bulletedList",
+            "numberedList",
+            "indent",
+            "outdent",
+            "|",
+            "imageUpload",
+            "insertTable",
+            "blockQuote",
+            "|",
+            "undo",
+            "redo",
+          ],
+          heading: {
+            options: [
+              {
+                model: "paragraph",
+                title: "Paragraph",
+                class: "ck-heading_paragraph",
+              },
+              {
+                model: "heading1",
+                view: "h1",
+                title: "Heading 1",
+                class: "ck-heading_heading1",
+              },
+              {
+                model: "heading2",
+                view: "h2",
+                title: "Heading 2",
+                class: "ck-heading_heading2",
+              },
+              {
+                model: "heading3",
+                view: "h3",
+                title: "Heading 3",
+                class: "ck-heading_heading3",
+              },
+              {
+                model: "heading4",
+                view: "h4",
+                title: "Heading 4",
+                class: "ck-heading_heading4",
+              },
+              {
+                model: "heading5",
+                view: "h5",
+                title: "Heading 5",
+                class: "ck-heading_heading5",
+              },
+              {
+                model: "heading6",
+                view: "h6",
+                title: "Heading 6",
+                class: "ck-heading_heading6",
+              },
+            ],
+          },
+        }}
+        ref={refEditor}
+        onChange={handleChange}
+      />
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
