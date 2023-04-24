4f76d6301c1fa78194c96febbc9979cbe5eeeb8f
Seokseong Jeon <seokseong@jeon.engineer>
2023-04-25 00:09:13 +0900

    Extract editor config (fix f3b31e6485dca548e39c262b6e9136a4d684e0c7)

--- src/App.tsx
+++ src/App.tsx
@@ -1,75 +1,10 @@
 import "./App.css";
 import "bulma/css/bulma.min.css";
-import type { EditorConfig } from "@ckeditor/ckeditor5-core";
 import { CKEditor } from "@ckeditor/ckeditor5-react";
 import ClassicEditor from "@ckeditor/ckeditor5-build-classic";
 import { createRef, useState } from "react";
+import editorConfig from "./editorConfig";
 
-const editorConfig: EditorConfig = {
-  toolbar: [
-    "heading",
-    "|",
-    "bold",
-    "italic",
-    "|",
-    "bulletedList",
-    "numberedList",
-    "indent",
-    "outdent",
-    "|",
-    "imageUpload",
-    "insertTable",
-    "blockQuote",
-    "|",
-    "undo",
-    "redo",
-  ],
-  heading: {
-    options: [
-      {
-        model: "paragraph",
-        title: "Paragraph",
-        class: "ck-heading_paragraph",
-      },
-      {
-        model: "heading1",
-        view: "h1",
-        title: "Heading 1",
-        class: "ck-heading_heading1",
-      },
-      {
-        model: "heading2",
-        view: "h2",
-        title: "Heading 2",
-        class: "ck-heading_heading2",
-      },
-      {
-        model: "heading3",
-        view: "h3",
-        title: "Heading 3",
-        class: "ck-heading_heading3",
-      },
-      {
-        model: "heading4",
-        view: "h4",
-        title: "Heading 4",
-        class: "ck-heading_heading4",
-      },
-      {
-        model: "heading5",
-        view: "h5",
-        title: "Heading 5",
-        class: "ck-heading_heading5",
-      },
-      {
-        model: "heading6",
-        view: "h6",
-        title: "Heading 6",
-        class: "ck-heading_heading6",
-      },
-    ],
-  },
-};
 
 function App() {
   const refEditor = createRef<CKEditor<ClassicEditor>>();
