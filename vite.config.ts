403da3110815d4bd8eb767ebcdcb75008caa2001
Seokseong Jeon <seokseong@jeon.engineer>
2023-04-28 00:22:50 +0900

    Add base URL (commented out)

--- vite.config.ts
+++ vite.config.ts
@@ -3,6 +3,7 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  base: 'https://somidad.github.io/tempub',
   plugins: [react()],
   optimizeDeps: {
     include: ['ckeditor5-custom-build/build/ckeditor'],
